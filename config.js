2cb2ef55dd83ee39a4bfb350e07e1393c91f556e
azizt91 <21195038@mhs.stmik-tegal.ac.id>
2025-08-26 20:42:05 +0700

    Revisi Teroos

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 // config.js - API Configuration
 const config = {
     // GANTI URL DI BAWAH INI DENGAN WEB APP URL DARI GOOGLE APPS SCRIPT ANDA
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbyQ38Yz7FcAo9jxa3hvXoP0vZsx45fiaaVH_a811BbI3Gqc8y0kBSkGhi-zORjm-2qH-Q/exec', // <-- GANTI DENGAN URL WEB APP ANDA
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbww1wHQDC9ZJv05jHadYPQ5Ck5-4Yy8VBC2USEZdAnEaiUwSA7WjC7y_S6rFD6vsAnLCQ/exec', // <-- GANTI DENGAN URL WEB APP ANDA
+    
     
     // Fungsi untuk mendapatkan URL API
     getApiUrl: function(endpoint) {
