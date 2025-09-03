fe7850b9b623c35bcaba045ce45bf92e209938e3
azizt91 <128366806+azizt91@users.noreply.github.com>
2025-09-03 11:49:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - API Configuration
 const config = {
     // GANTI URL DI BAWAH INI DENGAN WEB APP URL DARI GOOGLE APPS SCRIPT ANDA
-    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxJLeCl9405M-qTOYELrZMpXcDdvIG-P1hFLQavSEhitNMINxFcaHlYDxCknulGjT92Vw/exec', // <-- GANTI DENGAN URL WEB APP ANDA
+    API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzsFiAxovpMINDTXtiO96AEBmSh53eRJ4F71RxFDiGYyHRSclYv2s947XeP0YgcHusGuw/exec', // <-- GANTI DENGAN URL WEB APP ANDA
     
     
     // Fungsi untuk mendapatkan URL API
